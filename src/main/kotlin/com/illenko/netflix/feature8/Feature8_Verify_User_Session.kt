aa45df66a8e108bceb49c3de143ad5cd7a7f2777
illenko <kostiaillenko@gmail.com>
2023-01-05 21:49:20 +0200

    Netflix Feature #8 (minor formatting)

--- src/main/kotlin/com/illenko/netflix/feature8/Feature8_Verify_User_Session.kt
+++ src/main/kotlin/com/illenko/netflix/feature8/Feature8_Verify_User_Session.kt
@@ -1,9 +1,9 @@
 package com.illenko.netflix.feature8 // ktlint-disable filename
 
-class Stack<T> : Collection<T> {
+class Stack<T> {
+
     private var head: Node<T>? = null
-    public override var size: Int = 0
-        private set
+    private var size: Int = 0
 
     private class Node<T>(var value: T) {
         var next: Node<T>? = null
@@ -29,31 +29,20 @@ class Stack<T> : Collection<T> {
         return old.value
     }
 
-    override fun isEmpty(): Boolean {
-        return size == 0
-    }
+    fun isEmpty(): Boolean = size == 0
 
-    override fun contains(element: T): Boolean {
+    fun contains(element: T): Boolean {
         for (obj in this) {
             if (obj == element) return true
         }
         return false
     }
 
-    override fun containsAll(elements: Collection<T>): Boolean {
-        for (element in elements) {
-            if (!contains(element)) return false
-        }
-        return true
-    }
-
-    override fun iterator(): Iterator<T> {
+    operator fun iterator(): Iterator<T> {
         return object : Iterator<T> {
             var node = head
 
-            override fun hasNext(): Boolean {
-                return node != null
-            }
+            override fun hasNext(): Boolean = node != null
 
             override fun next(): T {
                 if (!hasNext()) throw NoSuchElementException()
