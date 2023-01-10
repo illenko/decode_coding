5288dc79cbd41e24d402c82b0af84e6a2d6de1ec
illenko <kostiaillenko@gmail.com>
2023-01-10 19:46:27 +0200

    Resolved Netflix DIY#5 (LRU)

--- src/main/kotlin/com/illenko/netflix/diys/diy5/DIY5_LRU.kt
+++ src/main/kotlin/com/illenko/netflix/diys/diy5/DIY5_LRU.kt
@@ -1,37 +1,37 @@
 package com.illenko.netflix.diys.diy5 // ktlint-disable filename
 
 internal class LRUCache(private var capacity: Int) {
-    // LinkedListNode holds key and value pairs
-    private var cache: HashMap<Int, LinkedListNode> = HashMap(capacity)
-    private var cacheVals: MyLinkedList = MyLinkedList()
+    
+    private var cache = HashMap<Int, LinkedListNode>(capacity)
+    private var cacheValues = MyLinkedList()
 
     fun Get(key: Int): Int {
         return if (cache.containsKey(key)) {
             val value = cache[key]!!.data
-            cacheVals.removeNode(cache[key])
-            cacheVals.insertAtTail(key, value)
-            cacheVals.tail?.data ?: -1
+            cacheValues.removeNode(cache[key])
+            cacheValues.insertAtTail(key, value)
+            cacheValues.tail?.data ?: -1
         } else {
             -1
         }
     }
 
     fun Set(key: Int, value: Int) {
-        if (cache.containsKey(key)) cacheVals.removeNode(cache[key]) else evictIfNeeded()
-        cacheVals.insertAtTail(key, value)
-        cache[key] = cacheVals.tail!!
+        if (cache.containsKey(key)) cacheValues.removeNode(cache[key]) else evictIfNeeded()
+        cacheValues.insertAtTail(key, value)
+        cache[key] = cacheValues.tail!!
     }
 
     private fun evictIfNeeded() {
-        if (cacheVals.size >= capacity) {
-            val node = cacheVals.removeHead()
+        if (cacheValues.size >= capacity) {
+            val node = cacheValues.removeHead()
             cache.remove(node?.key)
         }
     }
 
     fun print(): String {
         var result = ""
-        var node: LinkedListNode? = cacheVals.head
+        var node = cacheValues.head
         while (node != null) {
             result += "(" + node.key.toString() + "," + node.data.toString() + ")"
             node = node.next
