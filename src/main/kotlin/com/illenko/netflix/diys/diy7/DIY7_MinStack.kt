94395b5d847a1ac8c4d43295d25d86b1df5c7328
illenko <kostiaillenko@gmail.com>
2023-01-17 11:53:43 +0200

    Netflix DIY#7

--- src/main/kotlin/com/illenko/netflix/diys/diy7/DIY7_MinStack.kt
+++ src/main/kotlin/com/illenko/netflix/diys/diy7/DIY7_MinStack.kt
@@ -1,35 +1,25 @@
-package com.illenko.netflix.diys.diy7
+package com.illenko.netflix.diys.diy7 // ktlint-disable filename
 
-
-// ktlint-disable filename
-
-class MinStack(var maxSize: Int) {
+class MinStack(private var maxSize: Int) {
 
     private var mainStack: Stack<Int> = Stack(maxSize)
-    private var maximumStack: Stack<Int> = Stack(maxSize)
+    private var minStack: Stack<Int> = Stack(maxSize)
 
     fun pop(): Int {
-        // 1. Pop element from maxStack to make it sync with mainStack,
-        // 2. Pop element from mainStack and return that value
-        maximumStack.pop()
+        minStack.pop()
         val max = mainStack.top()
         mainStack.pop()
         return max ?: Int.MIN_VALUE
     }
 
-    // pushes value into the stack
     fun push(value: Int) {
-        // 1. Push value in mainStack and check value with the top value of maxStack
-        // 2. If value is greater than top, then push top in maxStack
-        // else push value in maxStack
         mainStack.push(value)
-        if (!maximumStack.isEmpty && maximumStack.top() ?: Int.MIN_VALUE < value) {
-            maximumStack.push(maximumStack.top()!!)
-        } else maximumStack.push(value)
+        if (!minStack.isEmpty && minStack.top()!! < value) {
+            minStack.push(minStack.top()!!)
+        } else minStack.push(value)
     }
 
-    // returns maximum value in O(1)
     fun min(): Int {
-        return maximumStack.top()!!
+        return minStack.top()!!
     }
 }
