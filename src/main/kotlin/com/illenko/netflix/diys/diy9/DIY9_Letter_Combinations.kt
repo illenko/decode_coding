c043271e6e452772187f1ca728d9e3e655d24e17
illenko <kostiaillenko@gmail.com>
2023-01-18 21:17:52 +0200

    Netflix DIY#9 (Back track initial)

---
+++ src/main/kotlin/com/illenko/netflix/diys/diy9/DIY9_Letter_Combinations.kt
@@ -0,0 +1,41 @@
+package com.illenko.netflix.diys.diy9 // ktlint-disable filename
+
+internal object Solution {
+    fun letterCombinations(digits: String): List<String> =
+        if (digits.isEmpty() || digits.contains('1')) listOf()
+        else backTrack(
+            0,
+            mutableListOf(),
+            digits.toCharArray(),
+            mapOf(
+                '2' to "abc",
+                '3' to "def",
+                '4' to "ghi",
+                '5' to "jkl",
+                '6' to "mno",
+                '7' to "pqrs",
+                '8' to "tuv",
+                '9' to "wxyz"
+            )
+        )
+
+    private fun backTrack(
+        index: Int,
+        path: MutableList<String>,
+        chars: CharArray,
+        values: Map<Char, String>
+    ): List<String> =
+        if (path.size == chars.size) {
+            path
+        } else {
+            val combinations = mutableListOf<String>()
+            values[chars[index]]?.let { value ->
+                value.toList().forEach {
+                    path.add(it.toString())
+                    combinations.addAll(backTrack(index + 1, path, chars, values))
+                    if (path.isNotEmpty()) path.removeLast()
+                }
+            }
+            combinations
+        }
+}
